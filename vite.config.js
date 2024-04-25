c1de7c1097080d8fe36ba1d35eb6b279d0605be6
Deltecharch <deltecharch@gmail.com>
2024-04-24 21:43:54 -0600

    agregando la base para que se vea

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://deltecharch.github.io/Pagina-Enlaces/"
 })
